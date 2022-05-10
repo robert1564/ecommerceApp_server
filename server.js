bbc53b03f9c36d8d6cd7d8ba75924559aab53d53
robert1564 <robertmaximus383@gmail.com>
2022-05-10 13:20:22 +0300

    traviss

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(morgan("dev"))
 // connect to mongoDB
 connectDB();
-console.log("test")
+console.log("testsss")
 // define routes and API
 app.use(express.json({extended:false}));
 app.use("/api/users", require("./routes/userApi"));
